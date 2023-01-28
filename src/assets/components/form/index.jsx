c3271c2d047eebc4d24a15508a809d58302f0639
André Brito <andree_goncalves@hotmail.com>
2023-01-28 00:13:18 -0300

    Forms

---
+++ src/assets/components/form/index.jsx
@@ -0,0 +1,52 @@
+import "./style.css";
+
+
+export function Form (){
+    return(
+        <div>
+            <form action="">
+                <fieldset className="infos">
+                    <legend>A N D R A R T</legend>
+                    <fieldset>
+                        <label htmlFor="">Coleção</label>
+                        <input type="text" name="" id="" />
+                    </fieldset>
+
+                    <fieldset>
+                        <label htmlFor="">Peça</label>
+                        <input type="text" name="" id="" />
+                    </fieldset>
+
+                    <fieldset>
+                        <label htmlFor="">Tamanho</label>
+                        <input type="text" name="" id="" />
+                    </fieldset>
+
+                    <fieldset>
+                        <label htmlFor="">Cor</label>
+                        <input type="text" name="" id="" />
+                    </fieldset>
+
+                
+                    <fieldset>
+                        <label htmlFor="">Sexo</label>
+                        <input type="text" name="" id="" />
+                    </fieldset>
+
+                
+                    <fieldset>
+                        <label htmlFor="">Cliente</label>
+                        <input type="text" name="" id="" />
+                    </fieldset>
+
+                
+                    <fieldset>
+                        <label htmlFor="">Valor</label>
+                        <input type="text" name="" id="" />
+                    </fieldset>
+
+                </fieldset>
+            </form>
+        </div>
+    )
+};
---
+++ src/pages/Home/index.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './style.css'
+import { Form } from '../../assets/components/form';
+
+export function Home() {
+  return (
+    <div>
+      <Form />
+    
+    </div>
+  );
+};
+
